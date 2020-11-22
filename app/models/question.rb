6b9644e22a2ff5487282aace4f129d3dc2466990
onlykir <kekelev.k@mail.ru>
2020-11-22 23:13:18 +0300

    Update validates

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :user
-  validates :text, :user, presence: true, length: { maximum: 255 }
+  validates :text, presence: true, length: { maximum: 255 }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,32 +4,20 @@ class User < ApplicationRecord
   ITERATIONS = 20000
   DIGEST = OpenSSL::Digest::SHA256.new
   EMAIL_VALID = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  USERNAME_VALID = /\A[a-zA-Z0-9_]+\z/i
-  has_many :questions, :dependent => :destroy
+  USERNAME_VALID = /\A[a-z0-9_]+\z/i
+  has_many :questions, dependent: :destroy
+
   validates :name, presence: true
-  validates :username, presence: true, uniqueness: { case_sensitive: false },
+  validates :username, presence: true, uniqueness: true,
             length: { maximum: 40 }, format: { with: USERNAME_VALID }
   validates :email, presence: true, uniqueness: { case_sensitive: false },
             format: { with: EMAIL_VALID }
 
   attr_accessor :password
 
-  validates_presence_of :password, on: :create
-  validates_confirmation_of :password
+  validates :password, presence: true, confirmation: true, on: :create
 
-  before_save :encrypt_password
-  before_save { self.email = email.downcase }
-  before_save { self.username = username.downcase }
-
-  def encrypt_password
-    if self.password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
-      )
-    end
-  end
+  before_save :data_preparation
 
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
@@ -42,5 +30,22 @@ class User < ApplicationRecord
     else
       nil
     end
+  end
+
+  private
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
+    end
+  end
+
+  def data_preparation
+    encrypt_password
+    self.email = email.downcase
   end
 end
