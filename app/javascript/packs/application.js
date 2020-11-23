6667c213eb9a74d52588c4a6af1a6879cdd183f8
onlykir <kekelev.k@mail.ru>
2020-11-24 00:11:44 +0300

    require JQuery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require_tree .
+
+import './add_question'
+
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
+require("jquery")
 require("channels")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
