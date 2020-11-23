f35cf9bae79a1f8f16b1fcc4d30758e9cfaec4a1
onlykir <kekelev.k@mail.ru>
2020-11-23 18:10:03 +0300

    Add count of posts block

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,15 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def declination_of_word(count, form_, form_a, form_ov)
+    return form_ov if (11..14).include?(count % 100)
+
+    last_num = count % 10
+
+    return form_ if last_num == 1
+    return form_a if (2..4).include?(last_num)
+    form_ov
+  end
+
 end
