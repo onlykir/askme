7bebeba5705a454a93641aa95f68e30e2f8a3ced
onlykir <kekelev.k@mail.ru>
2020-11-24 15:14:43 +0300

    security fix user_id

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -41,6 +41,10 @@ class QuestionsController < ApplicationController
     end
 
     def question_params
-      params.require(:question).permit(:user_id, :text, :answer)
+      if current_user.present? && params[:question][:user_id].to_i == current_user.id
+        params.require(:question).permit(:user_id, :text, :answer)
+      else
+        params.require(:question).permit(:user_id, :text)
+      end
     end
 end
