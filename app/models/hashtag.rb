998c22d87e5269677fccf3a9129d34a131bba01c
onlykir <kekelev.k@mail.ru>
2020-12-23 18:01:50 +0300

    Autocreate hashtags when question saves

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,4 +1,6 @@
 class Hashtag < ApplicationRecord
+  REGEXP = /#[[:word:]-]+/
+
   has_many :hashtag_questions, dependent: :destroy
   has_many :questions, through: :hashtag_questions
   validates :text, presence: true, uniqueness: true
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,5 +3,18 @@ class Question < ApplicationRecord
   has_many :hashtags, through: :hashtag_questions
   belongs_to :user
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', optional: true
+
   validates :text, presence: true, length: { maximum: 255 }
+
+  after_save_commit :create_hashtags
+
+  private
+
+  def create_hashtags
+    self.hashtags =
+      "#{text} #{answer}".downcase.
+        scan(Hashtag::REGEXP).
+        uniq.
+        map { |ht| Hashtag.find_or_create_by(text: ht.delete('#'))}
+  end
 end
