48271544c6791c5f92c8e2bb65e298a4407ef4bf
onlykir <kekelev.k@mail.ru>
2020-12-23 19:10:40 +0300

    Add link to hastag in question text

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,3 +1,7 @@
 module QuestionsHelper
-
+  def text_with_hashtag_link(text)
+    text.gsub(Hashtag::REGEXP) do |ht|
+      link_to ht, hashtag_path(ht.delete('#').downcase)
+    end.html_safe
+  end
 end
