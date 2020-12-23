41c0653f2354c684ae98d7a218e1f294eafc79df
onlykir <kekelev.k@mail.ru>
2020-12-23 19:00:40 +0300

    Add Hastag page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resource :sessions, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
-  resources :hashtags, only: :show, params: :text
+  resources :hashtags, only: :show, param: :text
 
   get 'sign_up' => 'users#new'
   get 'log_out' => 'sessions#destroy'
